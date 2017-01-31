420edde3f3e440f26d79283c5f37a766cc261bdb
Carlos Munoz Kampff and Maggie Harrington <maggie@>
2017-01-30 16:16:13 -0800

    add custom markers on google maps.

--- maps-test/mapscript.js
+++ maps-test/mapscript.js
@@ -1,38 +1,48 @@
 function initMap() {
-  var fruitsOfDiversity = {lat: 45.590499, lng: -122.711121};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    center: fruitsOfDiversity
-  });
-  var marker = new google.maps.Marker({
-    position: fruitsOfDiversity,
+
+var mapNeighbourhood = {
+    center: new google.maps.LatLng(45.590604, -122.711207),
+    zoom: 10,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+
+
+
+var map = new google.maps.Map(document.getElementById('map'), mapNeighbourhood);
+
+var mapFruitsOfDiversity = {
+    position: new google.maps.LatLng(45.590604, -122.711207),
     map: map
-  });
-///////copy paste
-// var mapOptions = {
-//     center: new google.maps.LatLng(45.590604, -122.711207),
-//     zoom: 12,
-//     mapTypeId: google.maps.MapTypeId.ROADMAP
-// };
-// var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-//
-// var markerOptions = {
-//     position: new google.maps.LatLng(45.590604, -122.711207),
-//     map: map
-// };
-// var marker = new google.maps.Marker(markerOptions);
-// marker.setMap(map);
-//
-// var infoWindowOptions = {
-//     content: 'Moscone Is Here! Fruits of Diversity Community Orchard'
-// };
-//
-// var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
-// google.maps.event.addListener(marker,'click',function(e){
-//
-//   infoWindow.open(map, marker);
-//
-// });
-////copy paste
+};
+
+var mapSabinCommunityOrchard = {
+    position: new google.maps.LatLng(45.553110, -122.646701),
+    map: map
+};
+
+var marker = new google.maps.Marker(mapFruitsOfDiversity);
+marker.setMap(map);
+var marker2 = new google.maps.Marker(mapSabinCommunityOrchard);
+marker.setMap(map);
+
+var infoWindowOptionsFOD = {
+    content: 'Fruits of Diversity Community Orchard <br>Plot stats: <br>plot size: big <br>fruits: apples '
+};
+var infoWindowOptionsSCO = {
+    content: 'Sabin Community Orchard <br>Plot stats: <br>plot size: big <br>fruits: apples '
+};
+
+var infoWindowFOD = new google.maps.InfoWindow(infoWindowOptionsFOD);
+google.maps.event.addListener(marker,'click',function(e){
+
+  infoWindowFOD.open(map, marker);
+
+});
+var infoWindowSCO = new google.maps.InfoWindow(infoWindowOptionsSCO);
+google.maps.event.addListener(marker2,'click',function(e){
+
+  infoWindowSCO.open(map, marker2);
+
+});
 
 }
