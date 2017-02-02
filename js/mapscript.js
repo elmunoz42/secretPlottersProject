d4c3dacca7effa82f27ff1750cab94a4cc7d3077
Maggie Harrington <maggie.harrington@gmail.com>
2017-02-02 14:11:27 -0800

    manual merge of maggie-solo and carlos-solo

--- js/mapscript.js
+++ js/mapscript.js
@@ -7,6 +7,12 @@ function SecretPlot(plotName, latitude, longitude, description, plotSize, produc
   this.plotProduce = produce;
   this.plotBlog = blog;
 }
+
+function BlogEntry(plotter, message) {
+  this.plotter = plotter;
+  this.message = message;
+}
+
 var secretPlotGCO = new SecretPlot("Gabriel Community Orchard", 45.470266, -122.720072, 'Located adjacent to Gabriel Community Garden at 4151 SW Canby St in Southwest Portland, Gabriel Community Orchard is home to more than 40 mature fruit trees, the perfect orchard for any apple lover. ', 'big', [' apples', ' other'], [{plotter: 'Bob', message:'Im way down here!'},{plotter: 'Tina', message: 'Ya me too!'}]);
 
 var secretPlotPCUCC = new SecretPlot("Parkrose Community United Church of Christ", 45.534277, -122.533966, 'Parkrose Community Orchard is our fifth and newest orchard. Located on the property of Parkrose Community United Church of Christ, it is home to about 30 fruit trees and dozens of understory and native plants.', 'big',[' apples', ' other'],[{plotter: 'Bob', message:'Mmm native plants!'},{plotter: 'Tina', message: 'Wow!'}]);
@@ -46,6 +52,8 @@ $(document).ready(function() {
     event.preventDefault();
     $("#add-profile").hide();
     $("#add-new-plot").hide();
+    $("#add-new-blog-post").hide();
+    $("#blog-posts").hide();
     $("#plot-title").show();
     $("#viewmap").show();
     initMap ();
@@ -59,6 +67,8 @@ $(document).ready(function() {
     $("#viewmap").hide();
     $(".find-plot-div").hide();
     $("#add-new-plot").hide();
+    $("#add-new-blog-post").hide();
+    $("#blog-posts").hide();
     $("#add-profile").show();
   })
   // create-profile-button
@@ -94,6 +104,9 @@ $(document).ready(function() {
     $("#plot-title").hide();
     $("#viewmap").hide();
     $(".find-plot-div").hide();
+    $("#add-new-blog-post").hide();
+    $("#blog-posts").hide();
+    $("#add-profile").hide();
     $("#add-new-plot").show();
 
   });
@@ -103,6 +116,7 @@ $(document).ready(function() {
     event.preventDefault();
     $("#plot-title").empty();
     $(".find-plot-div").hide();
+    $("#add-profile").hide();
     $("#blog-plotter1").text("");
     $("#blog-message1").text("");
     $("#blog-plotter2").text("");
@@ -140,6 +154,44 @@ $(document).ready(function() {
 
      var markerNew = new google.maps.Marker(mapNewSecretPlot);
 
+     var mapFruitsOfDiversity = {
+            position: new google.maps.LatLng(secretPlotFOD.latitude, secretPlotFOD.longitude),
+            map: map
+        };
+      var mapSabinCommunityOrchard = {
+          position: new google.maps.LatLng(secretPlotSCO.latitude, secretPlotSCO.longitude),
+          map: map
+      };
+      var mapGabrielCommunityOrchard = {
+          position: new google.maps.LatLng(secretPlotGCO.latitude, secretPlotGCO.longitude),
+          map: map
+      };
+      var mapGTCO = {
+          position: new google.maps.LatLng(secretPlotGTCO.latitude, secretPlotGTCO.longitude),
+          map: map
+      };
+      var mapPCUCC = {
+          position: new google.maps.LatLng(secretPlotPCUCC.latitude, secretPlotPCUCC.longitude),
+          map: map
+      };
+
+
+
+      var markerFOD = new google.maps.Marker(mapFruitsOfDiversity);
+      var markerSCO = new google.maps.Marker(mapSabinCommunityOrchard);
+      var markerGCO = new google.maps.Marker(mapGabrielCommunityOrchard);
+      var markerGTCO = new google.maps.Marker(mapGTCO);
+      var markerPCUCC = new google.maps.Marker(mapPCUCC);
+      markerGTCO.setMap(map);
+
+
+      var markers = [markerFOD, markerSCO, markerGCO, markerGTCO, markerPCUCC];
+
+  //
+
+      markers.push(markerNew);
+
+
      var infoWindowOptionsNew = {
          content: secretPlotNew.plotName + '<br>Plot stats: <br>plot size: ' + secretPlotNew.plotSize + ' <br>produce: ' + secretPlotNew.plotProduce,
      };
@@ -147,6 +199,10 @@ $(document).ready(function() {
      var infoWindowNew = new google.maps.InfoWindow(infoWindowOptionsNew);
      google.maps.event.addListener(markerNew,'click',function(e){
        infoWindowNew.open(map, markerNew);
+       $("#blog-plotter1").text("");
+       $("#blog-message1").text("");
+       $("#blog-plotter2").text("");
+       $("#blog-message2").text("");
        $(".find-plot-div").show();
        $("#plot-title").text(secretPlotNew.plotName);
        $("#plot-title2").text(secretPlotNew.plotName);
@@ -158,6 +214,114 @@ $(document).ready(function() {
        $("#plot-size").text(secretPlotNew.plotSize);
        $("#plot-produce").text(secretPlotNew.plotProduce);
      });
+
+     var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+
+    var infoWindowOptionsFOD = {
+        content: secretPlotFOD.plotName + '<br>Plot stats: <br>plot size: ' + secretPlotFOD.plotSize + ' <br>produce: ' + secretPlotFOD.plotProduce,
+    };
+    var infoWindowOptionsSCO = {
+        content: secretPlotSCO.plotName + '<br>Plot stats: <br>plot size: ' + secretPlotSCO.plotSize + ' <br>produce: ' + secretPlotSCO.plotProduce,
+    };
+    var infoWindowOptionsGCO = {
+        content: secretPlotGCO.plotName + '<br>Plot stats: <br>plot size: ' + secretPlotGCO.plotSize + ' <br>produce: ' + secretPlotGCO.plotProduce,
+    };
+    var infoWindowOptionsGTCO = {
+        content: secretPlotGTCO.plotName + '<br>Plot stats: <br>plot size: ' + secretPlotGTCO.plotSize + ' <br>produce: ' + secretPlotGTCO.plotProduce,
+    };
+    var infoWindowOptionsPCUCC = {
+        content: secretPlotPCUCC.plotName + '<br>Plot stats: <br>plot size: ' + secretPlotPCUCC.plotSize + ' <br>produce: ' + secretPlotPCUCC.plotProduce,
+    };
+    var infoWindowFOD = new google.maps.InfoWindow(infoWindowOptionsFOD);
+    google.maps.event.addListener(markerFOD,'click',function(e){
+      infoWindowFOD.open(map, markerFOD);
+      $(".find-plot-div").show();
+      $("#plot-title").text(secretPlotFOD.plotName);
+      $("#plot-title2").text(secretPlotFOD.plotName);
+      $("#plot-description").text(secretPlotFOD.plotDescription);
+      $("#blog-plotter1").text(secretPlotFOD.plotBlog[0].plotter);
+      $("#blog-message1").text(secretPlotFOD.plotBlog[0].message);
+      $("#blog-plotter2").text(secretPlotFOD.plotBlog[1].plotter);
+      $("#blog-message2").text(secretPlotFOD.plotBlog[1].message);
+      $("#plot-size").text(secretPlotFOD.plotSize);
+      $("#plot-produce").text(secretPlotFOD.plotProduce);
+    });
+    var infoWindowSCO = new google.maps.InfoWindow(infoWindowOptionsSCO);
+    google.maps.event.addListener(markerSCO,'click',function(e){
+      infoWindowSCO.open(map, markerSCO);
+      $(".find-plot-div").show();
+      $("#plot-title").text(secretPlotSCO.plotName);
+      $("#plot-title2").text(secretPlotSCO.plotName);
+      $("#plot-description").text(secretPlotSCO.plotDescription);
+      $("#blog-plotter1").text(secretPlotSCO.plotBlog[0].plotter);
+      $("#blog-message1").text(secretPlotSCO.plotBlog[0].message);
+      $("#blog-plotter2").text(secretPlotSCO.plotBlog[1].plotter);
+      $("#blog-message2").text(secretPlotSCO.plotBlog[1].message);
+      $("#plot-size").text(secretPlotSCO.plotSize);
+      $("#plot-produce").text(secretPlotSCO.plotProduce);
+    });
+    var infoWindowGCO = new google.maps.InfoWindow(infoWindowOptionsGCO);
+    google.maps.event.addListener(markerGCO,'click',function(e){
+      infoWindowGCO.open(map, markerGCO);
+      $(".find-plot-div").show();
+      $("#plot-title").text(secretPlotGCO.plotName);
+      $("#plot-title2").text(secretPlotGCO.plotName);
+      $("#plot-description").text(secretPlotGCO.plotDescription);
+      $("#blog-plotter1").text(secretPlotGCO.plotBlog[0].plotter);
+      $("#blog-message1").text(secretPlotGCO.plotBlog[0].message);
+      $("#blog-plotter2").text(secretPlotGCO.plotBlog[1].plotter);
+      $("#blog-message2").text(secretPlotGCO.plotBlog[1].message);
+      $("#plot-size").text(secretPlotGCO.plotSize);
+      $("#plot-produce").text(secretPlotGCO.plotProduce);
+    });
+    var infoWindowGTCO = new google.maps.InfoWindow(infoWindowOptionsGTCO);
+    google.maps.event.addListener(markerGTCO,'click',function(e){
+      infoWindowGTCO.open(map, markerGTCO);
+      $(".find-plot-div").show();
+      $("#plot-title").text(secretPlotGTCO.plotName);
+      $("#plot-title2").text(secretPlotGTCO.plotName);
+      $("#plot-description").text(secretPlotGTCO.plotDescription);
+      $("#blog-plotter1").text(secretPlotGTCO.plotBlog[0].plotter);
+      $("#blog-message1").text(secretPlotGTCO.plotBlog[0].message);
+      $("#blog-plotter2").text(secretPlotGTCO.plotBlog[1].plotter);
+      $("#blog-message2").text(secretPlotGTCO.plotBlog[1].message);
+      $("#plot-size").text(secretPlotGTCO.plotSize);
+      $("#plot-produce").text(secretPlotGTCO.plotProduce);
+    });
+    var infoWindowPCUCC = new google.maps.InfoWindow(infoWindowOptionsPCUCC);
+    google.maps.event.addListener(markerPCUCC,'click',function(e){
+      infoWindowPCUCC.open(map, markerPCUCC);
+      $(".find-plot-div").show();
+      $("#plot-title").text(secretPlotPCUCC.plotName);
+      $("#plot-title2").text(secretPlotPCUCC.plotName);
+      $("#plot-description").text(secretPlotPCUCC.plotDescription);
+      $("#blog-plotter1").text(secretPlotPCUCC.plotBlog[0].plotter);
+      $("#blog-message1").text(secretPlotPCUCC.plotBlog[0].message);
+      $("#blog-plotter2").text(secretPlotPCUCC.plotBlog[1].plotter);
+      $("#blog-message2").text(secretPlotPCUCC.plotBlog[1].message);
+      $("#plot-size").text(secretPlotPCUCC.plotSize);
+      $("#plot-produce").text(secretPlotPCUCC.plotProduce);
+    });
+
+
+
+
+
+    $("#new-plot-name").val("");
+    $("#new-latitude").val("");
+    $("#new-longitude").val("");
+    $("#new-description").val("");
+    $("#new-plot-size").val("");
+    $("#new-produce").val("");
+    $("#new-blog-user").val("");
+    $("#new-blog").val("");
+
+
+
+
+
+
   });
   // form new-plot-submit
 
@@ -173,9 +337,51 @@ $(document).ready(function() {
     event.preventDefault();
     $("#plot-title").hide();
     $("#viewmap").hide();
+    $(".find-plot-div").hide();
+    $("#add-new-plot").hide();
+    $("#add-profile").hide();
     console.log("comment-on-plot");
-  });
+    $("#add-new-blog-post").show();
 
+    $("form#new-blog-post-submit").submit(function(event) {
+      event.preventDefault();
+      $("#plot-title").empty();
+      $(".find-plot-div").hide();
+      $("#add-profile").hide();
+      $("#blog-plotter1").text("");
+      $("#blog-message1").text("");
+      $("#blog-plotter2").text("");
+      $("#blog-message2").text("");
+
+
+      var newBlogPostUser = $("#new-blog-post-user").val();
+      var newBlogPost = $("#new-blog-post").val();
+
+      var newPlotBlog = [];
+      var blogEntryNew = new BlogEntry (newBlogPostUser, newBlogPost);
+      console.log(blogEntryNew);
+      newPlotBlog.push(blogEntryNew);
+      console.log(newPlotBlog);
+
+      $("#blog-posts").show();
+
+
+
+      $("#new-blog-entry").prepend('<p class="blog-poster">' +
+                                    blogEntryNew.plotter + '</p>' +
+                                    '<blockquote class="blog-entry">' + blogEntryNew.message + '</blockquote>'
+
+      );
+      //append new blog entry
+
+      $("#new-blog-post-user").val("");
+      $("#new-blog-post").val("");
+    });
+// new blog post submit
+
+
+  });
+//message board button
 
 });
 
