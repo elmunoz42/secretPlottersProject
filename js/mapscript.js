18765b4facb6e1630f64bba3432bcdca8d0fb3fc
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-02-01 22:52:57 -0800

    add two new locations and wider map view to showcase clusters.

--- js/mapscript.js
+++ js/mapscript.js
@@ -9,15 +9,19 @@ function SecretPlot(plotName, latitude, longitude, description, plotSize, produc
 }
 var secretPlotGCO = new SecretPlot("Gabriel Community Orchard", 45.470266, -122.720072, 'Located adjacent to Gabriel Community Garden at 4151 SW Canby St in Southwest Portland, Gabriel Community Orchard is home to more than 40 mature fruit trees, the perfect orchard for any apple lover. ', 'big', [' apples', ' other'], [{plotter: 'Bob', message:'Im way down here!'},{plotter: 'Tina', message: 'Ya me too!'}]);
 
-var secretPlotPCUCC = new SecretPlot("Parkrose Community United Church of Christ", 45.534277, -122.533966, 'Parkrose Community Orchard is our fifth and newest orchard. Located on the property of Parkrose Community United Church of Christ, it is home to about 30 fruit trees and dozens of understory and native plants.', 'big',[' apples', ' other'],[{plotter: 'Bob', message:'Im way down here!'},{plotter: 'Tina', message: 'Ya me too!'}]);
+var secretPlotPCUCC = new SecretPlot("Parkrose Community United Church of Christ", 45.534277, -122.533966, 'Parkrose Community Orchard is our fifth and newest orchard. Located on the property of Parkrose Community United Church of Christ, it is home to about 30 fruit trees and dozens of understory and native plants.', 'big',[' apples', ' other'],[{plotter: 'Bob', message:'Mmm native plants!'},{plotter: 'Tina', message: 'Wow!'}]);
 
-var secretPlotSCO = new SecretPlot("Sabin Community Orchard", 45.553035, -122.647525, 'Fourteen new fruit trees were planted in 2010 and 2011, to accompany two existing cherry trees and an apple tree. In 2011 we established under-story plantings around each fruit tree, water catching swales along the slope, sheet mulch, edging, and signs at both entrances.', 'big', [' cherries', ' apples'], [{plotter: 'Steve', message:'I can has cherries!'},{plotter: 'Tina', message: 'Ya me too!'}]);
+var secretPlotSCO = new SecretPlot("Sabin Community Orchard", 45.553035, -122.647525, 'Fourteen new fruit trees were planted in 2010 and 2011, to accompany two existing cherry trees and an apple tree. In 2011 we established under-story plantings around each fruit tree, water catching swales along the slope, sheet mulch, edging, and signs at both entrances.', 'big', [' cherries', ' apples'], [{plotter: 'Steve', message:'I can has cherries!'},{plotter: 'Tina', message: 'Watch our for Ceiling-Cat!'}]);
 
-var secretPlotGTCO = new SecretPlot("Green Thumb Community Orchard",45.562162, -122.644807, "Owned by Portland Parks and Recreation, and co-operatively managed by Portland Community Gardens and the Portland Fruit Tree Project, the Green Thumb Orchard is a historic orchard that hosts a great deal of activities. PFTP's Green Thumb Stewards maintain the orchard's trees, and share in the harvest through our harvesting parties.", 'big', ['fruit'], [{plotter: 'Bob', message:'Im way down here!'},{plotter: 'Tina', message: 'Ya me too!'}]);
+var secretPlotGTCO = new SecretPlot("Green Thumb Community Orchard",45.562162, -122.644807, "Owned by Portland Parks and Recreation, and co-operatively managed by Portland Community Gardens and the Portland Fruit Tree Project, the Green Thumb Orchard is a historic orchard that hosts a great deal of activities. PFTP's Green Thumb Stewards maintain the orchard's trees, and share in the harvest through our harvesting parties.", 'big', ['fruit'], [{plotter: 'Bob', message:'Im way down here!'},{plotter: 'Tina', message: 'Ya so low!'}]);
 
 var secretPlotFOD = new SecretPlot("Fruits of Diversity Community Orchard", 45.590446, -122.711207, 'Currently, the orchard is home to over 40 fruit trees, dozens of edible shrubs, and countless understory and companion plants and will continue to grow each year! ', ' big', [' fruits', ' edible shrubs' ], [{plotter: 'Bob', message:'I love this garden!'},{plotter: 'Tina', message: 'Ya me too!'}]);
 
-var secretPlots= [secretPlotPCUCC, secretPlotGCO, secretPlotGTCO, secretPlotSCO, secretPlotFOD ];
+var secretPlotECO = new SecretPlot("Engleston Community Orchard", 42.314182, -71.101638, "Situated on a parcel at 195 Boylston Street, Egleston Community Orchard (ECO) is a neighbor-led effort for sustainable urban agriculture and community green space, promoting environmental justice and community ownership of land in Egleston Square. ECO hopes to continue to engage the full diversity of Egleston Square, including youth, and frequently hosts cultural events and workshops open to the general public. Find out more about ECO.", 'big', [' fruits', ' berries', 'herbs', 'rain water'], [{plotter: 'Carlos', message:'I miss my Boston peeps!'},{plotter: 'Orion', message: 'Come get dirty with us!'}])
+
+var secretPlotBNC = new SecretPlot("Boston Nature Center", 42.288211, -71.101274, "In partnership with the Boston Nature Center, we are building a food forest in stages, focusing on a few centerpiece projects each year that offer valuable educational demonstration opportunities. We have spent this past growing season enriching the health of the soil through sheet mulching and the planting of nitrogen-fixing plants. We have also completed several Year 1 projects, including:", 'big', [' fruits', ' berries', 'herbs', 'leafy greens'], [{plotter: 'Carlos', message:'Happy wedding BoBo so sorry I missed it!'},{plotter: 'Bonnie', message: 'Spankings will continue until morale improves!'}])
+
+var secretPlots= [secretPlotPCUCC, secretPlotGCO, secretPlotGTCO, secretPlotSCO, secretPlotFOD, secretPlotECO];
 
 /// sorts secretPlots Array by latitude to match labels.
 secretPlots.sort(function(a, b){
@@ -117,8 +121,8 @@ function initMap() {
 }
 function initialize () {
 var mapNeighbourhood = {
-    center: new google.maps.LatLng(45.470266, -122.720072),
-    zoom: 7,
+    center: new google.maps.LatLng(38.151461, -95.076235),
+    zoom: 3,
     mapTypeId: google.maps.MapTypeId.ROADMAP
 };
 
@@ -144,18 +148,28 @@ var mapPCUCC = {
     position: new google.maps.LatLng(secretPlotPCUCC.latitude, secretPlotPCUCC.longitude),
     map: map
 };
+var mapECO = {
+  position: new google.maps.LatLng(secretPlotECO.latitude, secretPlotECO.longitude),
+  map: map
+}
+var mapBNC = {
+  position: new google.maps.LatLng(secretPlotBNC.latitude, secretPlotBNC.longitude),
+  map: map
+}
 
 var markerFOD = new google.maps.Marker(mapFruitsOfDiversity);
 var markerSCO = new google.maps.Marker(mapSabinCommunityOrchard);
 var markerGCO = new google.maps.Marker(mapGabrielCommunityOrchard);
 var markerGTCO = new google.maps.Marker(mapGTCO);
 var markerPCUCC = new google.maps.Marker(mapPCUCC);
+var markerECO = new google.maps.Marker(mapECO);
+var markerBNC = new google.maps.Marker(mapBNC);
 markerGTCO.setMap(map);
 
 
 
 
-var markers = [markerFOD, markerSCO, markerGCO, markerGTCO, markerPCUCC];
+var markers = [markerFOD, markerSCO, markerGCO, markerGTCO, markerPCUCC, markerECO, markerBNC];
 var markerCluster = new MarkerClusterer(map, markers,
     {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 
@@ -173,6 +187,12 @@ var infoWindowOptionsGTCO = {
 };
 var infoWindowOptionsPCUCC = {
     content: secretPlotPCUCC.plotName + '<br>Plot stats: <br>plot size: ' + secretPlotPCUCC.plotSize + ' <br>produce: ' + secretPlotPCUCC.plotProduce,
+};
+var infoWindowOptionsECO = {
+    content: secretPlotECO.plotName + '<br>Plot stats: <br>plot size: ' + secretPlotECO.plotSize + ' <br>produce: ' + secretPlotECO.plotProduce,
+};
+var infoWindowOptionsBNC = {
+    content: secretPlotBNC.plotName + '<br>Plot stats: <br>plot size: ' + secretPlotBNC.plotSize + ' <br>produce: ' + secretPlotBNC.plotProduce,
 };
 var infoWindowFOD = new google.maps.InfoWindow(infoWindowOptionsFOD);
 google.maps.event.addListener(markerFOD,'click',function(e){
@@ -243,6 +263,34 @@ google.maps.event.addListener(markerPCUCC,'click',function(e){
   $("#blog-message2").text(secretPlotPCUCC.plotBlog[1].message);
   $("#plot-size").text(secretPlotPCUCC.plotSize);
   $("#plot-produce").text(secretPlotPCUCC.plotProduce);
+});
+var infoWindowECO = new google.maps.InfoWindow(infoWindowOptionsECO);
+google.maps.event.addListener(markerECO,'click',function(e){
+  infoWindowECO.open(map, markerECO);
+  $(".find-plot-div").show();
+  $("#plot-title").text(secretPlotECO.plotName);
+  $("#plot-title2").text(secretPlotECO.plotName);
+  $("#plot-description").text(secretPlotECO.plotDescription);
+  $("#blog-plotter1").text(secretPlotECO.plotBlog[0].plotter);
+  $("#blog-message1").text(secretPlotECO.plotBlog[0].message);
+  $("#blog-plotter2").text(secretPlotECO.plotBlog[1].plotter);
+  $("#blog-message2").text(secretPlotECO.plotBlog[1].message);
+  $("#plot-size").text(secretPlotECO.plotSize);
+  $("#plot-produce").text(secretPlotECO.plotProduce);
+});
+var infoWindowBNC = new google.maps.InfoWindow(infoWindowOptionsBNC);
+google.maps.event.addListener(markerBNC,'click',function(e){
+  infoWindowBNC.open(map, markerBNC);
+  $(".find-plot-div").show();
+  $("#plot-title").text(secretPlotBNC.plotName);
+  $("#plot-title2").text(secretPlotBNC.plotName);
+  $("#plot-description").text(secretPlotBNC.plotDescription);
+  $("#blog-plotter1").text(secretPlotBNC.plotBlog[0].plotter);
+  $("#blog-message1").text(secretPlotBNC.plotBlog[0].message);
+  $("#blog-plotter2").text(secretPlotBNC.plotBlog[1].plotter);
+  $("#blog-message2").text(secretPlotBNC.plotBlog[1].message);
+  $("#plot-size").text(secretPlotBNC.plotSize);
+  $("#plot-produce").text(secretPlotBNC.plotProduce);
 });
 
 
